26b0578d612beac063962ca4fd8deb6c09c5c61f
Seaya-do <67862904+Seaya-do@users.noreply.github.com>
2022-01-03 13:55:32 +0900

    2022/01/02

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+import re
+
+a = b = 1
+count = 0
+
+while count < 11:
+    c = a + b
+    # 1자리씩 나눠서 각 자리의 합을 구함
+    sum = 0
+    for d in str(c):
+        sum += int(d)
+    if c % sum == 0:
+        # 나누어서 떨어지면 출력
+        print(c)
+        count += 1
+    a, b = b, c
